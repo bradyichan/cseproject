7fd481337087076e36141d50c0bca735fafdb92a
Brady <bradychan@MacBook-Pro.local>
2025-11-20 13:39:37 -0500

    remove unused imports

--- frontend/swap-and-sell-team22/src/pages/LogIn.tsx
+++ frontend/swap-and-sell-team22/src/pages/LogIn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 interface LogInProps {
   setIsAuthenticated: (val: boolean) => void;
--- frontend/swap-and-sell-team22/src/pages/SignUp.tsx
+++ frontend/swap-and-sell-team22/src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 interface SignUpProps {
   setIsAuthenticated: (val: boolean) => void;
