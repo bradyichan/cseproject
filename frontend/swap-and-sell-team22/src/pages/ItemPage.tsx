948846cd9d39f5728c0942bdf33e066dc8b3be5c
Brady <bradychan@MacBook-Pro.local>
2025-11-22 22:44:14 -0500

    linting

--- frontend/swap-and-sell-team22/src/pages/ItemPage.tsx
+++ frontend/swap-and-sell-team22/src/pages/ItemPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, FormEvent } from "react";
+import { useEffect, useState, type FormEvent } from "react";
 import { useParams, Link } from "react-router-dom";
 import "../App.css";
 
@@ -76,6 +76,7 @@ export default function ItemPage() {
   // Open bid modal
   // -----------------------------
   function handleOpenBidModal() {
+    if (!item) return;
     setBidError("");
 
     // Prefill with min acceptable bid
